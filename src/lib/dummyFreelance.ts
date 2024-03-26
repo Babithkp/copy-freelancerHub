328a8b28b407275edd55dde9b17fa7dcfd9627a9
Babithkp <babithkullachetti44@gmail.com>
2024-03-26 22:07:53 +0530

    finish touch

--- src/lib/dummyFreelance.ts
+++ src/lib/dummyFreelance.ts
@@ -22,7 +22,7 @@ export const dummyFreelance = (category: string) => {
           imageUrl:
             "/images/20170805_093502_636375411570945345_guruImgLarge_2ebdefde-bbe7-46c8-8a43-de8febdc8532.avif",
           address: "Chelsea, OK, USA",
-          pay: "Past Earnings",
+          pay: "180,000",
           name: "Ashley Boswell",
           tag: [
             "Medical",
@@ -37,7 +37,7 @@ export const dummyFreelance = (category: string) => {
           imageUrl:
             "/images/Arun_637227661339530961_guruImgLarge_3429529e-2cf6-46e8-b66a-245421803c8e.avif",
           address: "Mumbai, Maharashtra, India",
-          pay: "Past Earnings",
+          pay: "10,000",
           name: "Arun Kumar (Certified GTD Trainer)",
           tag: [
             "Business Analysis",
@@ -50,7 +50,7 @@ export const dummyFreelance = (category: string) => {
           imageUrl:
             "https://res.cloudinary.com/gurucom/image/upload/f_auto,w_128,h_128,dpr_2/v1679495317/pimg/FreelancerFiles/4/745/4745079/etezjdinjepkxqwpurpt.jpg",
           address: "Havant, England, United Kingdom",
-          pay: "1,768",
+          pay: "17,680",
           name: "Martin Jackson",
           tag: [
             "Business Analysis",
@@ -66,6 +66,7 @@ export const dummyFreelance = (category: string) => {
           id:5,
           imageUrl: "/images/pzxb4pejakj8u0rwywe7.avif",
           address: "Mexico, Nuevo Leon, Mexico",
+          pay: "21,000",
           name: "Yvone Castillo",
           tag: ["3D Design", "3D Modeling", "3D Rendering", "Building Design"],
         },
@@ -74,7 +75,7 @@ export const dummyFreelance = (category: string) => {
           imageUrl:
             "https://res.cloudinary.com/gurucom/image/upload/f_auto,w_128,h_128,dpr_2/pimg/2/184/2184858/PHOTO%20ALI_ABIDI_636358240520549748_guruImgLarge_961dcf55-1fa2-4a52-8007-108e04a90c3c.jpg",
           address: "Bou-Salem, Jendouba, Tunisia",
-          pay: "2,962",
+          pay: "34,000",
           name: "ALI ABIDI",
           tag: ["3D Design", "3D Modeling", "3D Rendering", "Building Design"],
         },
@@ -124,7 +125,7 @@ export const dummyFreelance = (category: string) => {
           imageUrl:
             "/images/headshotlulu_635338001201004201_guruImgLarge_dd61030f-d940-43be-870e-e3c1366c52f1.avif",
           address: "katy, TX, USA",
-
+          pay: "76,000",
           name: "Leo Valk",
           tag: ["Benefits", "Consultant", "Management", "WordPress"],
         },
@@ -132,6 +133,7 @@ export const dummyFreelance = (category: string) => {
           id:11,
           imageUrl: "/images/xejsruhf95ekwv1cmlib.avif",
           address: "Lahore, Punjab, Pakistan",
+          pay: "25,524",
           name: "M. Azam Khan",
           verified: "ID Verified",
           tag: ["GitHub", "LinkedIn", "Onboarding", "Skype"],
@@ -141,6 +143,7 @@ export const dummyFreelance = (category: string) => {
           imageUrl: "/images/ob29qrbp3sx8zkhquwpl.avif",
           address: "Indore, Madhya Pradesh, India",
           name: "PeopleCrop",
+          pay: "7,000",
           tag: ["Benefits Design", "IT Recruitment", "Onboarding", "Payroll"],
         },
       ];
@@ -151,6 +154,7 @@ export const dummyFreelance = (category: string) => {
           imageUrl: "/images/jpv8wttyewfn7lgbjjhk.avif",
           address: "Ajman, Ajman, United Arab Emirates",
           name: "Shweta Kulkarni1",
+          pay: "29,830",
           tag: ["Drafting", "Construction", "Training", "Rate Contracts"],
         },
         {
@@ -159,6 +163,8 @@ export const dummyFreelance = (category: string) => {
             "/images/BoxLogicSep201915_637097624163401196_guruImgLarge_d85c9113-0e8d-48f7-b3e0-25e47ae48e19.avif",
           address: "Southampton, England, United Kingdom",
           name: "Nishit Nisudan",
+          pay: "60,000",
+
           tag: [
             "Logistics",
             "Logistics Consulting",
@@ -172,7 +178,7 @@ export const dummyFreelance = (category: string) => {
             "/images/Linkedin Photo_636831174243587906_guruImgLarge_eead0e25-f58d-4e4f-9b7f-1855190e8554.avif",
           address: "Ronkonkoma, NY, USA",
           name: "Jonathan Riker",
-          pay: "150",
+          pay: "15,000",
           tag: ["Consultant", "ERP", "Programming", "Oracle Hyperion"],
         },
       ];
